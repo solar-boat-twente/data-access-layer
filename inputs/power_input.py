b7f075438557981287b29ba7787963d16dedea36
Eelco Eikelboom <e.eikelboom@hellebrekers.nl>
2019-05-02 10:48:57 +0200

    added connection status

--- inputs/power_input.py
+++ inputs/power_input.py
@@ -20,7 +20,7 @@ class PowerInputDef(Enum):
     direction_forward = Input(index=9, type=DataType.bit, modbus_ref=7)
     direction_backward = Input(index=10, type=DataType.bit, modbus_ref=8)
 
-    mode_text = Output(type=DataType.string, modbus_ref=40010)  # TODO CHANGE THIS WHEN MODE NUMBER CHANGES
+    # mode_text = Output(type=DataType.string, modbus_ref=40010)  # Text currently hardcoded in HMI
     mode_1_active = Output(type=DataType.bit, modbus_ref=0)     # TODO CHANGE THIS WHEN MODE NUMBER CHANGES
     mode_2_active = Output(type=DataType.bit, modbus_ref=1)     # TODO CHANGE THIS WHEN MODE NUMBER CHANGES
     mode_3_active = Output(type=DataType.bit, modbus_ref=2)     # TODO CHANGE THIS WHEN MODE NUMBER CHANGES
@@ -29,14 +29,13 @@ class PowerInputDef(Enum):
     battery_danger = Output(type=DataType.bit, modbus_ref=4)    # TODO CHANGE THIS WHEN BATTERY TEMP. CHANGES
     driver_danger = Output(type=DataType.bit, modbus_ref=5)     # TODO CHANGE THIS WHEN DRIVER TEMP. CHANGES
     motor_danger = Output(type=DataType.bit, modbus_ref=6)      # TODO CHANGE THIS WHEN MOTOR TEMP. CHANGES
+    connection_status = Output(type=DataType.int, modbus_ref=40012)
 
 
 class PowerInputInterpreter(DataInterpreter):
 
     def __init__(self, enum: Type[Enum]):
         super().__init__(enum)
-
-        self.set(PowerInputDef.mode_text, 'FLY')
         self.set(PowerInputDef.mode_1_active, 1)
         self.set(PowerInputDef.mode_2_active, 0)
         self.set(PowerInputDef.mode_3_active, 0)
@@ -44,3 +43,4 @@ class PowerInputInterpreter(DataInterpreter):
         self.set(PowerInputDef.battery_danger, 0)
         self.set(PowerInputDef.driver_danger, 0)
         self.set(PowerInputDef.motor_danger, 0)
+        self.set(PowerInputDef.connection_status, 2)
